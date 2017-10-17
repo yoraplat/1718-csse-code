9408ea1e34653827f0850e3cec7e84759147e168
yoraplat <yoraplat@student.arteveldehs.be>
2017-10-17 15:42:25 +0200

    Prime numbers

---
+++ primeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            bool  isPrime = true;
+
+
+            if (input > 100){
+                Console.WriteLine("Input must be 100 or lower");
+            } else {
+                for (int i = 1; i <= input; i++){
+                    for (int j = 2; j <= input; j++){
+                        if (i != j && i % j == 0){
+                            isPrime = false;
+                        }
+                    }
+
+                    if (isPrime){
+                        Console.WriteLine(i + " is prime");
+                    }
+                    isPrime = true;
+                }
+            }
+
+
+        }
+    }
+}
