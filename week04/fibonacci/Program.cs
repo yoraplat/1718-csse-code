951ddab9c834a571f1eb048ea2c7b447c4648d5a
yoraplat <yoraplat@student.arteveldehs.be>
2017-10-23 16:56:05 +0200

    First commit

---
+++ week04/fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            int [] arr = new int[input];
+            arr[0] = 0;
+            arr[1] = 1;
+            Console.WriteLine((arr[0] + arr[1]));
+            
+            // Fibonacci
+
+            for (int i = 2; i <= input ;i++){
+                int nextInt = (arr[i -2] + arr[i -1]);
+
+                arr[i] = nextInt;
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ week04/primeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace primeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number?");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+
+            
+
+        bool isPrime(int number){
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            for (int i = 2; i <= Math.Ceiling(Math.Sqrt(number)); ++i){
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+
+            for (int j = 0; j <= num1; j++){
+                if (isPrime(j)){
+                    Console.WriteLine( j + " is een priemgetal");
+                } else {
+                    Console.WriteLine(j + " is geen priemgetal");
+                }
+            }
+        }
+    }
+}
